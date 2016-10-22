dd7bb891a00d945552b5d88aab0edb7ad33b0114
Matthew Chung <matthewchung@Matthews-MacBook-Pro.local>
2016-10-22 06:57:30 -0400

    this is my UI

---
+++ sorinasprogram.py
@@ -0,0 +1,9 @@
+name = raw_input ('Please enter name: ');
+if name=='sorina':
+    ann = raw_input('Please enter passkey: ')
+    if(ann=='121813'):
+        print 'I love you :)'
+    else:
+        print 'Incorrect user. Program terminated.'
+else:
+    print 'Incorrect user. Program terminated.'
